adacc6488eb55f2723d015565d3fe21a156cc4cb
Rustam Zokirov <59933997+Rustam-Z@users.noreply.github.com>
2023-03-06 21:08:00 +0500

    [Token] Refresh token tests written (#2)
    * [Token] Negative test cases added
    
    * [Token] Refresh token tests added

--- tests/token/test_token.py
+++ tests/token/test_token.py
@@ -2,14 +2,14 @@
 Test user authentication. Access token and refresh token.
 """
 import pytest
-from faker import Faker
 
 from api.api_response import APIResponse
+from api.http_client import HTTPClient
 from api.requests.token_api import TokenAPI
-from api.responses.token_model import TokenErrorResponse
 from core.config.users import get_random_user
 from core.enums.auth import Auth
 from core.helpers.jwt_helpers import decode_jwt
+import data
 
 
 class TestToken:
@@ -42,14 +42,14 @@ class TestToken:
         AND response body contains access and refresh tokens.
         """
         email, password = get_random_user(Auth.SUPERUSER)
-        password = Faker().password()
+        password = data.fake.password()
 
         response, model = TokenAPI(client).login(email=email, password=password)
         APIResponse(response).check_status(401)
 
     @pytest.mark.parametrize('email, password', [
-        (Faker().email(), Faker().password()),
-        (Faker().name(), Faker().password()),  # Wrong email format.
+        (data.fake.email(), data.fake.password()),
+        (data.fake.name(), data.fake.password()),  # Wrong email format.
     ])
     def test_wrongEmailAndWrongPassword_returnsError(self, client, email, password):
         response, model = TokenAPI(client).login(email=email, password=password)
@@ -58,8 +58,8 @@ class TestToken:
         assert model.error.get('detail') == 'No active account found with the given credentials'
 
     @pytest.mark.parametrize('email, password, error', [
-        (' ', Faker().password(), {"email": ["This field may not be blank."]}),
-        (Faker().password(), ' ', {"password": ["This field may not be blank."]}),
+        (' ', data.fake.password(), {"email": ["This field may not be blank."]}),
+        (data.fake.password(), ' ', {"password": ["This field may not be blank."]}),
         (' ', ' ', {"email": ["This field may not be blank."], "password": ["This field may not be blank."]}),
     ])
     def test_emptyFields_returnsError(self, client, email, password, error):
@@ -67,3 +67,31 @@ class TestToken:
 
         APIResponse(response).check_status(400)
         assert model.error == error, 'Error content is not matching.'
+
+
+class TestRefreshToken:
+    @staticmethod
+    def get_refresh_token(client: HTTPClient) -> str:
+        email, password = get_random_user(Auth.SUPERUSER)
+        response, model = TokenAPI(client).login(email=email, password=password)
+        return model.data.refresh
+
+    def test_validRefreshToken_returnsToken(self, client):
+        refresh_token = self.get_refresh_token(client)
+        response, model = TokenAPI(client).refresh_token(refresh=refresh_token)
+
+        APIResponse(response).check_status(200)
+
+        old_refresh_jwt_data = decode_jwt(refresh_token)
+        new_access_jwt_data = decode_jwt(model.data.access)
+        new_refresh_jwt_data = decode_jwt(model.data.refresh)
+        assert old_refresh_jwt_data.get('user_id') == \
+               new_access_jwt_data.get('user_id') == \
+               new_refresh_jwt_data.get('user_id')
+
+    def test_wrongRefreshToken_returnsError(self, client):
+        refresh_token = data.fake.jwt_token()
+        response, model = TokenAPI(client).refresh_token(refresh=refresh_token)
+
+        APIResponse(response).check_status(401)
+        assert model.error.get('detail') == 'Token is invalid or expired'
