bfa738cc8c4c643982f5e8a03de45d45bcef304c
rustam-z <zokirovrustam202@gmail.com>
2023-03-09 00:55:54 +0500

    [TAF] added token caching to fixture.users

--- core/helpers/jwt_helper.py
+++ core/helpers/jwt_helper.py
@@ -1,15 +1,22 @@
+import time
+
 import jwt
 
 
 def decode_jwt(token: str) -> dict:
     """
     Function that reads data encoded into JWT.
-
-    Args:
-        token: Any JWT (JSON Web Token).
-
-    Returns: data that is encoded into token.
-
     """
     data = jwt.decode(token, options={'verify_signature': False})
     return data
+
+
+def is_jwt_expired(token: str, offset: int = 30) -> bool:
+    """
+    Function that validates that JWT token is not expired.
+    """
+    key = 'exp'
+    token_expiration_time = decode_jwt(token)[key]
+    current_time = int(time.time())
+    expiration_time = (token_expiration_time - current_time - offset)
+    return expiration_time < 0
--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,10 @@ from core.config import CONFIG
 from core.config.users import get_random_user
 from core.enums.environments import Environment
 from core.enums.users import User
+from core.helpers.jwt_helper import is_jwt_expired
 from core.http_client import HTTPClient
+
+CACHED_ACCESS_TOKENS = {}  # email: access_token
 
 
 @pytest.hookimpl
@@ -56,9 +59,23 @@ def users(request):
     else:
         # Update headers with Authorization header
         email, password = get_random_user(user_type)
-        response, model = TokenAPI(client).login(email=email, password=password)
-        access_token = model.data.access
+        access_token = get_access_token(client, email, password)
         headers = {
             'Authorization': 'Bearer ' + access_token,
         }
         client.update_headers(headers)
+
+
+def get_access_token(client: HTTPClient, email: str, password: str) -> str:
+    access_token = CACHED_ACCESS_TOKENS.get(email)  # Check cache
+    is_cached_access_token_expired = True
+
+    if access_token is not None:
+        is_cached_access_token_expired = is_jwt_expired(access_token)
+
+    if access_token is None or is_cached_access_token_expired:
+        response, model = TokenAPI(client).login(email=email, password=password)
+        access_token = model.data.access
+        CACHED_ACCESS_TOKENS[email] = access_token
+
+    return access_token
