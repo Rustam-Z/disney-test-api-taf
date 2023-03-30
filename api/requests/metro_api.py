057559c7dc2e103d88977f9e79e59eb75063de87
Rustam-Z <zokirovrustam202@gmail.com>
2023-03-30 10:39:02 +0500

    [Metro] Tests automated for metro CRUD - create, delete

---
+++ api/requests/metro_api.py
@@ -0,0 +1,74 @@
+from api.enums.params import Param
+from api.enums.sections import Section
+from api.responses.metro_model import (CreateMetroSuccessResponse,
+                                       UpdateMetroSuccessResponse,
+                                       GetMetroSuccessResponse,
+                                       GetAllMetrosSuccessResponse,
+                                       )
+from api.responses.response_models import ErrorResponse
+from core.http_client import HTTPClient
+
+
+class MetroAPI:
+    METRO = '/metro/'
+
+    def __init__(self, client: HTTPClient):
+        self.client = client
+        self.params = {
+            Param.SECTION.value: Section.METRO.value
+        }
+
+    def get_all_metros(self) -> tuple:
+        path = self.METRO
+        response = self.client.get(path, params=self.params)
+
+        if response.status_code in range(200, 300):
+            model = GetAllMetrosSuccessResponse(**response.json())
+        else:
+            model = ErrorResponse(**response.json())
+
+        return response, model
+
+    def get_metro(self, id: int) -> tuple:
+        path = f'{self.METRO}{id}'
+        response = self.client.get(path, params=self.params)
+
+        if response.status_code in range(200, 300):
+            model = GetMetroSuccessResponse(**response.json())
+        else:
+            model = ErrorResponse(**response.json())
+
+        return response, model
+
+    def create_metro(self, data: dict) -> tuple:
+        path = self.METRO
+        response = self.client.post(path, data=data, params=self.params)
+
+        if response.status_code in range(200, 300):
+            model = CreateMetroSuccessResponse(**response.json())
+        else:
+            model = ErrorResponse(**response.json())
+
+        return response, model
+
+    def update_metro(self, id: int, data: dict) -> tuple:
+        path = f'{self.METRO}{id}'
+        response = self.client.patch(path, data=data, params=self.params)
+
+        if response.status_code in range(200, 300):
+            model = UpdateMetroSuccessResponse(**response.json())
+        else:
+            model = ErrorResponse(**response.json())
+
+        return response, model
+
+    def delete_metro(self, id: int) -> tuple:
+        path = f'{self.METRO}{id}'
+        response = self.client.delete(path, params=self.params)
+
+        if response.status_code in range(200, 300):
+            model = None
+        else:
+            model = ErrorResponse(**response.json())
+
+        return response, model
