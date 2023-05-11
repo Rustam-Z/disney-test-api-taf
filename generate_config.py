e03501e80f7a097828c4a8252585b185d76acc3c
Rustam-Z <zokirovrustam202@gmail.com>
2023-05-12 01:36:30 +0500

    [CI] Adding env variables

--- generate_config.py
+++ generate_config.py
@@ -6,6 +6,12 @@ import yaml
 import os
 
 
+print(os.environ.get('DEV_FACILITY_ADMIN_EMAIL'))
+print(os.environ.get('DEV_FACILITY_DRIVER_EMAIL'))
+print(os.environ.get('DEV_FACILITY_USER_EMAIL'))
+print(os.environ.get('PASSWORD'))
+
+
 def main() -> None:
     with open('config-template.yaml', 'r') as f:
         template = yaml.safe_load(f)
