183d6fd15ef991bc5d6891ede334c267c8df3d94
Rustam-Z <zokirovrustam202@gmail.com>
2023-06-05 18:16:30 +0500

    [CI] Changed args to env variables

--- send_notification.py
+++ send_notification.py
@@ -1,30 +1,25 @@
 """
 Script to send email after test execution in CI/CD pipeline.
 """
-
+import os
 from datetime import datetime
 import threading
 import csv
 import smtplib
 import ssl
 from email.message import EmailMessage
-import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--username', type=str, required=True)
-parser.add_argument('--password', type=str, required=True)
-parser.add_argument('--receivers', type=str, nargs='+')
-parser.add_argument('--env', type=str, required=True)
-parser.add_argument('--run_number', type=str, required=True)
-parser.add_argument('--event_name', type=str, required=True)
-parser.add_argument('--workflow', type=str, required=True)
-parser.add_argument('--actor', type=str, required=True)
-args = parser.parse_args()
 
-# Define email sender and receivers.
-EMAIL_SENDER = args.username
-EMAIL_PASSWORD = args.password
-EMAIL_RECEIVERS = args.receivers
+print(os.listdir('allure-report'))
+
+EMAIL_SENDER = os.environ.get('EMAIL_SENDER')
+EMAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+EMAIL_RECEIVERS = os.environ.get('EMAIL_RECEIVERS')
+ENV = os.environ.get('ENV')
+RUN_NUMBER = os.environ.get('RUN_NUMBER')
+EVENT_NAME = os.environ.get('EVENT_NAME')
+WORKFLOW = os.environ.get('WORKFLOW')
+ACTOR = os.environ.get('ACTOR')
 
 # Email subject and body templates.
 SUBJECT = 'Disney Test Execution Report - {env} Environment - API Regression - {time}'
@@ -41,7 +36,7 @@ def get_metrics() -> str:
     """
     Read metrics data from allure report and transforms the data.
     """
-    file_path = '.output/allure-history/data/behaviors.csv'
+    file_path = 'allure-report/data/behaviors.csv'
     data = []
     with open(file_path, 'r') as csv_file:
         csv_reader = csv.reader(csv_file)
@@ -64,12 +59,12 @@ def send_email(email_receiver):
     time = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
     test_metrics = get_metrics()
 
-    subject = SUBJECT.format(time=time, env=args.env)
+    subject = SUBJECT.format(time=time, env=ENV)
     body = BODY.format(
-        run_number=args.run_number,
-        event_name=args.event_name,
-        workflow=args.workflow,
-        actor=args.actor,
+        run_number=RUN_NUMBER,
+        event_name=EVENT_NAME,
+        workflow=WORKFLOW,
+        actor=ACTOR,
         test_metrics=test_metrics
     )
 
@@ -91,7 +86,8 @@ def send_email(email_receiver):
 
 def main():
     threads = []
-    for email_receiver in EMAIL_RECEIVERS:
+    for email_receiver in EMAIL_RECEIVERS.split(','):
+        print(f">>> Sending email to {email_receiver}...")
         thread = threading.Thread(target=send_email, args=(email_receiver,))
         threads.append(thread)
         thread.start()
