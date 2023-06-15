9d64cf19041ecf0535ba1b5512e8ada1575dd49d
Rustam-Z <zokirovrustam202@gmail.com>
2023-06-15 23:21:31 +0500

    [Dashboard] Defined inventory apis

--- api/endpoints/dashboards/inventory_api.py
+++ api/endpoints/dashboards/inventory_api.py
@@ -13,14 +13,110 @@ class InventoryDashboardAPI:
             Param.SECTION.value: Section.DASHBOARD.value
         }
 
-    def get_current_inventory(self, **kwargs) -> tuple:
-        ...
+    def get_current_inventory(self, params: dict = None, **kwargs) -> tuple:
+        """
+        Params:
+        facility: int
+        inventory_base_filter: metro_config/item_type
+        category_id: int
+        item_type_ids[]: int
+        item_type_ids[]: int
+        metro_config_ids[]: int
+        metro_config_ids[]: int
+        filter_by_all: bool
+        """
+        if params is None:
+            params = {}
 
-    def get_daily_inventory(self, **kwargs) -> tuple:
-        ...
+        params.update(self.params)  # Create query string params dict.
 
-    def get_daily_goals(self, **kwargs) -> tuple:
-        ...
+        path = f'{self.DASHBOARD}current-inventory'
+        response = self.client.get(path, params=params, **kwargs)
+        response_payload = response.content
 
-    def get_metro_locations(self, **kwargs) -> tuple:
-        ...
+        if response.status_code == 200:
+            response_payload = response.json()
+        elif response.status_code in range(400, 500):
+            response_payload = ErrorResponse(**response.json())
+
+        return response, response_payload
+
+    def get_daily_inventory(self, params: dict = None, **kwargs) -> tuple:
+        """
+        Params:
+        facility: int
+        daily_inventory_type: cart_build/delivered
+        inventory_base_filter: metro_config/item_type
+        frequency: daily/weekly/monthly
+        category_id: int
+        item_type_ids[]: int
+        item_type_ids[]: int
+        metro_config_ids[]: int
+        metro_config_ids[]: int
+        filter_by_all: bool
+        """
+        if params is None:
+            params = {}
+
+        params.update(self.params)  # Create query string params dict.
+
+        path = f'{self.DASHBOARD}daily-inventory'
+        response = self.client.get(path, params=params, **kwargs)
+        response_payload = response.content
+
+        if response.status_code == 200:
+            response_payload = response.json()
+        elif response.status_code in range(400, 500):
+            response_payload = ErrorResponse(**response.json())
+
+        return response, response_payload
+
+    def get_daily_goals(self, params: dict = None, **kwargs) -> tuple:
+        """
+        Params:
+        facility: int
+        inventory_base_filter: metro_config/item_type
+        frequency: daily/weekly/monthly
+        category_id: int
+        item_type_ids[]: int
+        item_type_ids[]: int
+        metro_config_ids[]: int
+        metro_config_ids[]: int
+        filter_by_all: bool ???
+        """
+        if params is None:
+            params = {}
+
+        params.update(self.params)  # Create query string params dict.
+
+        path = f'{self.DASHBOARD}daily-inventory-goal'
+        response = self.client.get(path, params=params, **kwargs)
+        response_payload = response.content
+
+        if response.status_code == 200:
+            response_payload = response.json()
+        elif response.status_code in range(400, 500):
+            response_payload = ErrorResponse(**response.json())
+
+        return response, response_payload
+
+    def get_metro_locations(self, params: dict = None, **kwargs) -> tuple:
+        """
+        Params:
+        facility
+        """
+        if params is None:
+            params = {}
+
+        params.update(self.params)  # Create query string params dict.
+
+        path = f'{self.DASHBOARD}metro-location'
+        response = self.client.get(path, params=params, **kwargs)
+        response_payload = response.content
+
+        if response.status_code == 200:
+            response_payload = response.json()
+        elif response.status_code in range(400, 500):
+            response_payload = ErrorResponse(**response.json())
+
+        return response, response_payload
